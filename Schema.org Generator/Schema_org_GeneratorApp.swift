f9ae3c04b572a83fa45fdbe3683dca92de169a25
David Rodriguez <david@enyutech.com>
2025-03-05 13:49:01 -0500

    Initial Commit

---
+++ Schema.org Generator/Schema_org_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Schema_org_GeneratorApp.swift
+//  Schema.org Generator
+//
+//  Created by David Rodriguez on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Schema_org_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
